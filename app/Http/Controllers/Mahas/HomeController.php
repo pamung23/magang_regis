698703482c4264715952398d42e1eb5f16803295
Hp - GK <andiniexo150@gmail.com>
2023-08-03 01:23:14 +0700

    test

--- app/Http/Controllers/Mahas/HomeController.php
+++ app/Http/Controllers/Mahas/HomeController.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function index()
-    { //
+    {
         $mahasiswa = auth()->guard('mahasiswa')->user();
 
         // Ambil data surat terbaru untuk mahasiswa tertentu
